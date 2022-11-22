3362328f8d8aedc1b6549862995fe5ad4057ee97
Franco <francoperaltagg@gmail.com>
2022-11-22 19:53:15 -0300

    constructor

--- classesAndObjects.java
+++ classesAndObjects.java
@@ -2,8 +2,9 @@ public class HelloWorld {
     
     public static void main(String[] args){
 
-        Car c1 = new Car();
-        System.out.println(c1.color);
+        Car c1 = new Car("white", 1999);
+
+        c1.drive();
     }
 
 }
@@ -11,12 +12,18 @@ public class HelloWorld {
 public class Car {
 
     //Atributes
-    public int modelYear = 2023;
-    public String color = "Red";
+    public int modelYear;
+    public String color;
 
     //Methods
     public void drive(){
         System.out.println("Driving");
+    }
+
+    //Constructor
+    public Car(String color, int modelYear){
+        this.color = color;
+        this.modelYear = modelYear;
     }
 
 }
